853cf95e766bb98876f1e97da897f0dd2107b3d2
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-02-27 20:19:10 -0600

    first commit

---
+++ kevinHernandezGarza_lab3.cpp
@@ -0,0 +1,18 @@
+//******************************************************************
+// Author: Kevin Hernandez-Garza
+// Assignment Number: Programming Assignment "2"
+// File Name: hernandezGarza.Lab2.cpp
+// Course/Section: COSC 1337 Section -009
+// Due Date: 2/25/23
+// Instructor: Dr.Haytham Mohamed
+//
+// This program determines which company's four divisions(Northeast, Southeast, Northwest, and Southwest) had the greatest sales for a quarter. It includes two functions that are called by the main function.
+//  ******************************************************************
+
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
